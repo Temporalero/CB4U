17c7b3eed5c4d0bb225ac4e12b73bf220d8273a0
Kewitz Colina <kewitz.colina@gmail.com>
2021-07-02 15:04:45 -0400

    [ADD] Imagen en maintenance equipment

---
+++ maintenance_cb4u/__manifest__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Maintenance CB4U",
+    'version': "14.0.1",
+    'author': "Kewitz Colina",
+    'website': "https://github.com/colinak",
+    'summary': "Maintenance",
+    'sequence': 10,
+    'description': "Maintenance",
+    'category': "Maintenance",
+    'depends': [
+        'base',
+        'maintenance',
+    ],
+    'data': [
+        'views/maintenance_view.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+
+}
+
+
+
