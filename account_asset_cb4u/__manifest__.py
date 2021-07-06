1b318efb09328b7660dcd4961a52fa031c14e714
Kewitz Colina <kewitz.colina@gmail.com>
2021-07-06 16:04:34 -0400

    [WIP] Modulo account_asset_cb4u

---
+++ account_asset_cb4u/__manifest__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Account Asset CB4U",
+    'version': "14.0.1",
+    'author': "Kewitz Colina",
+    'website': "https://github.com/colinak",
+    'summary': "asset",
+    'sequence': 10,
+    'description': "asset",
+    'category': "asset",
+    'depends': [
+        'base',
+        'account_asset',
+    ],
+    'data': [
+        'views/account_asset_view.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+
+}
+
+
+
+
